7576e940c12adc24340b28407ccacaf7f22b6f0d
barkotenicholas <barkotenicholas@gmail.com>
2022-03-13 14:48:47 +0300

    Added test

--- src/main/java/models/pojos/DepartmentNews.java
+++ src/main/java/models/pojos/DepartmentNews.java
@@ -1,8 +1,36 @@
 package models.pojos;
 
+import java.util.Objects;
+
 public class DepartmentNews  extends News{
+
     public static final String DATABASE ="General";
-    public DepartmentNews(String content, String author) {
+    private int departmentId;
+
+    public DepartmentNews(String content, String author ,int departmentId) {
         super(content, author);
+        this.departmentId = departmentId;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        DepartmentNews that = (DepartmentNews) o;
+        return departmentId == that.departmentId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), departmentId);
     }
 }
---
+++ src/test/java/models/pojos/DepartmentNewsTest.java
@@ -0,0 +1,35 @@
+package models.pojos;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Department news")
+class DepartmentNewsTest {
+
+
+    @DisplayName("check if department initializes correctly")
+    @Test
+    void departmentNews_instantiatesCorrectly(){
+        DepartmentNews test = new DepartmentNews("this is this","Nickson",12);
+        assertTrue(test instanceof DepartmentNews);
+    }
+
+    @DisplayName("Department id is returned ")
+    @Test
+    void getDepartmentId() {
+        DepartmentNews test = new DepartmentNews("this is this","Nickson",12);
+        assertEquals(12,test.getDepartmentId());
+    }
+
+    @DisplayName("Check if two instances of department are equal ")
+    @Test
+    void testEquals() {
+        DepartmentNews testOne = new DepartmentNews("this is this","Nickson",12);
+        DepartmentNews testTwo = new DepartmentNews("this is this","Nickson",12);
+
+        assertTrue(testOne.equals(testTwo));
+    }
+}
