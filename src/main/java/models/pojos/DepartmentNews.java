4c3c64e3981c435b05f810f7b57502f7e11fb4ac
barkotenicholas <barkotenicholas@gmail.com>
2022-03-13 14:38:52 +0300

    added Department and general news classes

---
+++ src/main/java/models/pojos/DepartmentNews.java
@@ -0,0 +1,8 @@
+package models.pojos;
+
+public class DepartmentNews  extends News{
+    public static final String DATABASE ="General";
+    public DepartmentNews(String content, String author) {
+        super(content, author);
+    }
+}
---
+++ src/main/java/models/pojos/GeneralNews.java
@@ -0,0 +1,14 @@
+package models.pojos;
+
+public class GeneralNews extends News{
+
+    private final static String DATABASE = "General";
+
+    public GeneralNews(String content, String author) {
+        super(content, author);
+
+    }
+
+
+
+}
